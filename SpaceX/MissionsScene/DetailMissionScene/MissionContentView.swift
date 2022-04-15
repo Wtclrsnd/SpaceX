62caae6b4790ad3b283637312e8f4567d009eb81
Emil Shpeklord <emil42661@gmail.com>
2022-04-15 11:59:06 +0300

    detail mission view is now fixed. adding a keyboard support

--- SpaceX/MissionsScene/DetailMissionScene/MissionContentView.swift
+++ SpaceX/MissionsScene/DetailMissionScene/MissionContentView.swift
@@ -8,13 +8,12 @@
 import UIKit
 
 class MissionContentView: UIView {
-    private var scrollView: UIScrollView!
-
     private lazy var descriptionTextView: UITextView = {
         let textView = UITextView()
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.textAlignment = .left
         textView.text = "description"
+        textView.contentInset = UIEdgeInsets(top: 20, left: 10, bottom: 20, right: 10)
         textView.font = .systemFont(ofSize: 28)
         return textView
     }()
@@ -36,36 +35,20 @@ class MissionContentView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        configureScrollView()
         setUpUI()
     }
 
     private func setUpUI() {
         backgroundColor = .systemBackground
-        scrollView.backgroundColor = .systemBackground
 
-        scrollView.addSubview(descriptionTextView)
+        addSubview(descriptionTextView)
 
-        descriptionTextView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        descriptionTextView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 15).isActive = true
-        descriptionTextView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 50).isActive = true
-        descriptionTextView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -50).isActive = true
+        descriptionTextView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        descriptionTextView.topAnchor.constraint(
+            equalTo: self.safeAreaLayoutGuide.topAnchor
+        ).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50).isActive = true
         descriptionTextView.widthAnchor.constraint(equalToConstant: self.frame.width - 30).isActive = true
-
-        scrollView.contentSize = descriptionTextView.bounds.size
-    }
-
-    private func configureScrollView() {
-        scrollView = UIScrollView(frame: .zero)
-        scrollView.contentInsetAdjustmentBehavior = .never
-
-        addSubview(scrollView)
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-
-        scrollView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor).isActive = true
-        scrollView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        scrollView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
     }
 }
