80727ffdea4a847e1e8748de2e76a5ce400de98a
Emil Shpeklord <emil42661@gmail.com>
2022-04-15 11:46:13 +0300

    refactored uilabel in textview and the text doesnt appear

--- SpaceX/MissionsScene/DetailMissionScene/MissionContentView.swift
+++ SpaceX/MissionsScene/DetailMissionScene/MissionContentView.swift
@@ -10,19 +10,18 @@ import UIKit
 class MissionContentView: UIView {
     private var scrollView: UIScrollView!
 
-    private lazy var descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        label.text = "description"
-        label.font = .systemFont(ofSize: 28)
-        return label
+    private lazy var descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.textAlignment = .left
+        textView.text = "description"
+        textView.font = .systemFont(ofSize: 28)
+        return textView
     }()
 
     var mission: Mission.InitForm.ViewModel? {
         didSet {
-            descriptionLabel.text = mission?.missionDescription ?? ""
+            descriptionTextView.text = mission?.missionDescription ?? ""
         }
     }
 
@@ -45,15 +44,15 @@ class MissionContentView: UIView {
         backgroundColor = .systemBackground
         scrollView.backgroundColor = .systemBackground
 
-        scrollView.addSubview(descriptionLabel)
+        scrollView.addSubview(descriptionTextView)
 
-        descriptionLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        descriptionLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 15).isActive = true
-        descriptionLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 50).isActive = true
-        descriptionLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -50).isActive = true
-        descriptionLabel.widthAnchor.constraint(equalToConstant: self.frame.width - 30).isActive = true
+        descriptionTextView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 15).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 50).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -50).isActive = true
+        descriptionTextView.widthAnchor.constraint(equalToConstant: self.frame.width - 30).isActive = true
 
-        scrollView.contentSize = descriptionLabel.bounds.size
+        scrollView.contentSize = descriptionTextView.bounds.size
     }
 
     private func configureScrollView() {
